899cc83501c9b32a5476947cd7ddad526c0b2d2f
Jorge Obiols <jorge.obiols@gmail.com>
2021-03-07 20:24:26 -0300

    [FIX] travis

--- l10n_ar_fe_qr/models/res_partner.py
+++ l10n_ar_fe_qr/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
@@ -9,18 +8,17 @@
 from openerp import models, api, _
 #from openerp.tools import float_repr
 from openerp.exceptions import Warning as UserError
-
+@api.multi
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.one
     def get_id_number_sanitize(self):
         """ Sanitize the identification number. Return the digits/interger value of
             the identification number
         """
-        id_number = self.document_number.replace('.', '').replace(' ', '')
-        id_number = id_number.replace('-', '')
-        if not id_number.isdigit():
-            raise UserError(_('El numero de cuit es invalido para %s') % self.name)
-        res = int(id_number)
-        return res
+        for rec in self:
+            id_number = rec.document_number.replace('.', '').replace(' ', '')
+            id_number = id_number.replace('-', '')
+            if not id_number.isdigit():
+                raise UserError(_('El numero de cuit es invalido para %s') % rec.name)
+            return int(id_number)
